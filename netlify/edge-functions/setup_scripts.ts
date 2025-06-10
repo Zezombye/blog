bd734c2dcf41ac062568a508c3556b7d82b72c5d
Zezombye <zezombye>
2025-06-10 13:32:08 +0200

    test edge functions

---
+++ netlify/edge-functions/setup_scripts.ts
@@ -0,0 +1,14 @@
+import type { Context, Config } from "@netlify/edge-functions";
+
+export default async (request: Request, context: Context) => {
+    if (request.headers.get("user-agent")?.match(/\b(Windows)?PowerShell\b/i)) {
+        return new URL("/windows-setup.ps1", request.url);
+    }
+    if (request.headers.get("user-agent")?.match(/^Wget\//)) {
+        return new URL("/.bashrc", request.url);
+    }
+    return;
+};
+export const config: Config = {
+    path: "/",
+};
